e9964afd97f5ec3bc9dd711e635fdd52f2311bb0
Troy Presley <troy.presley@gmail.com>
2022-06-05 00:38:04 -1000

    add commonly used xstream extras to exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 'use strict'
 
+// export sygnal core functions
 export { default as component, ABORT } from "./component"
 export { default as collection } from "./collection"
 export { default as switchable } from "./switchable"
 export { default as processForm } from "./extra/processForm"
 export { default as run } from './extra/run'
 export { default as classes } from './extra/classes'
+
+// export dom helper functions (h, div, ...)
 export * from '@cycle/dom'
+
+// export xstream and most used extra operators
 export { default as xs } from 'xstream'
+export { default as debounce } from "xstream/extra/debounce"
+export { default as throttle } from 'xstream/extra/throttle'
+export { default as delay } from "xstream/extra/delay"
+export { default as dropRepeats } from "xstream/extra/dropRepeats"
+export { default as sampleCombine } from 'xstream/extra/sampleCombine'
