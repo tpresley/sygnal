86c00913b71019c5c52501d8601e3d0b794c36a9
Troy Presley <troy.presley@gmail.com>
2026-03-21 20:15:00 -1000

    fix more imports

--- src/cycle/dom/snabbdom.ts
+++ src/cycle/dom/snabbdom.ts
@@ -10,23 +10,23 @@
  */
 
 // Core
-export {h, fragment as HFragment} from 'snabbdom/build/h';
-export {init} from 'snabbdom/build/init';
-export type {Options} from 'snabbdom/build/init';
-export {toVNode} from 'snabbdom/build/tovnode';
-export {vnode} from 'snabbdom/build/vnode';
-export type {VNode, VNodeData} from 'snabbdom/build/vnode';
-import {Fragment as _Fragment} from 'snabbdom/build/jsx';
+export {h, fragment as HFragment} from 'snabbdom/build/h.js';
+export {init} from 'snabbdom/build/init.js';
+export type {Options} from 'snabbdom/build/init.js';
+export {toVNode} from 'snabbdom/build/tovnode.js';
+export {vnode} from 'snabbdom/build/vnode.js';
+export type {VNode, VNodeData} from 'snabbdom/build/vnode.js';
+import {Fragment as _Fragment} from 'snabbdom/build/jsx.js';
 
 // Tag Fragment so we can identify it even after minification mangles Function.name
 (_Fragment as any).__sygnalFragment = true;
 export const Fragment = _Fragment;
 
 // Module type
-export type {Module} from 'snabbdom/build/modules/module';
+export type {Module} from 'snabbdom/build/modules/module.js';
 
 // Modules (excluding styleModule — see ./styleModule.ts)
-export {classModule} from 'snabbdom/build/modules/class';
-export {propsModule} from 'snabbdom/build/modules/props';
-export {attributesModule} from 'snabbdom/build/modules/attributes';
-export {datasetModule} from 'snabbdom/build/modules/dataset';
+export {classModule} from 'snabbdom/build/modules/class.js';
+export {propsModule} from 'snabbdom/build/modules/props.js';
+export {attributesModule} from 'snabbdom/build/modules/attributes.js';
+export {datasetModule} from 'snabbdom/build/modules/dataset.js';
